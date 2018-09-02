246e1c36428adb0b9f8d74f6c64ebfef8051e3ef
Oscar O'Brien <o.obrien2@student.unimelb.edu.au>
2018-09-02 23:46:36 +1000

    added data variables, constructor and setters

--- app/src/main/java/models/Message.java
+++ app/src/main/java/models/Message.java
@@ -1,7 +1,46 @@
 package models;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * contains a message
  */
 public class Message {
+
+    private Long id;
+    private Date date;
+    private String content;
+    private User sender;
+    private User recipient;
+    private Boolean isRead;
+
+
+    public Message(User sender, User recipient, Date date, String content){
+        setSender(sender);
+        setRecipient(recipient);
+        setDate(date);
+        setContent(content);
+        setIsRead(false);
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setIsRead(Boolean read) {
+        isRead = read;
+    }
 }
