541e26cfa8c4b829e6b4854c3b01fbca6e2d227a
William Liandri <williamliandri@gmail.com>
2018-09-06 12:50:27 +1000

    Adding map activity

--- app/src/main/java/com/example/gavv/my_groww_project/MainActivity.java
+++ app/src/main/java/com/example/gavv/my_groww_project/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.gavv.my_groww_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    public void goToMap() {
+
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMap();
+            }
+        });
     }
 
 }
